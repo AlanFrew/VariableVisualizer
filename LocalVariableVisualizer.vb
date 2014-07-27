99ade08b8e317ca94c7c15481b9c910de56b5c69
AlanFrew <alanmfrew@gmail.com>
2014-07-27 01:48:57 -0400

    Version 1.0
    Version 1.0

---
+++ LocalVariableVisualizer.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class LocalVariableVisualizer
+    ''' <summary>
+    ''' Outputs a list of local variables, followed by a line-wise summary of what local variables are in use
+    ''' </summary>
+    ''' <param name="suppressSubsetsOfSize">Causes sections of the specified size or less to be condensed into a '.'</param>
+    ''' <remarks>Does not support multiple variable declarations per line</remarks>
+    Public Shared Sub Visualize(ByVal functionText As String, Optional ByVal suppressSubsetsOfSize As Integer = 0, Optional ByVal startingLineNumber As Integer = 0)
+        Console.WriteLine()
+
+        Dim localVariables = FindLocalVariables(functionText, startingLineNumber)
+        PrintLocalVariables(localVariables)
+        Dim sections = FindSections(localVariables)
+        PrintSections(sections, suppressSubsetsOfSize)
+
+        Console.WriteLine()
+    End Sub
+
+    Private Shared Function FindLocalVariables(ByVal functionText As String, Optional ByVal startingLineNumber As Integer = 0) As Dictionary(Of String, LocalVariableProperties)
+        Dim lines As String() = functionText.Split(Environment.NewLine)
+        Dim foundLocals As New Dictionary(Of String, LocalVariableProperties)
+        Dim lineNumber As Integer = startingLineNumber
+        Dim fullMatchRegex As String = String.Empty
+
+        For Each line As String In lines
+            line = line.Replace(Constants.vbLf, "")
+            If fullMatchRegex <> String.Empty Then
+                For Each usage As Match In Regex.Matches(line, fullMatchRegex)
+                    foundLocals(usage.Groups(0).ToString()).endingLineNum = lineNumber
+                Next
+            End If
+
+            Dim match = Regex.Match(line, "Dim\s+(\w+)\s.*")
+            If match.Length <> 0 Then
+                Dim matchName = match.Groups(1).ToString()
+                foundLocals.Add(matchName, New LocalVariableProperties() With
+                                       {.name = matchName, .startingLine = match.Groups(0).ToString(), .startingLineNum = lineNumber})
+                fullMatchRegex = (fullMatchRegex & "|(" & matchName & "(?=[^\w]))|" & matchName & "$").Trim("|")
+            End If
+            lineNumber += 1
+        Next
+
+        For Each localVariable In foundLocals
+            If localVariable.Value.endingLineNum = 0 Then
+                localVariable.Value.endingLineNum = lineNumber
+            End If
+        Next
+
+        Return foundLocals
+    End Function
+
+    Private Shared Sub PrintLocalVariables(ByVal localVariables As Dictionary(Of String, LocalVariableProperties))
+        Console.WriteLine("Local Variables:")
+
+        For Each localVariable In localVariables
+            Console.WriteLine(localVariable.Key & " (" & localVariable.Value.startingLineNum & "-" & localVariable.Value.endingLineNum & ")")
+        Next
+
+        Console.WriteLine()
+    End Sub
+
+    Private Shared Function FindSections(ByVal localVariables As Dictionary(Of String, LocalVariableProperties)) As List(Of SectionOfScope)
+        Dim scopeSections As New List(Of SectionOfScope)
+        Dim startingAndEndingLinesSet As New SortedSet(Of Integer)
+
+        'Calculate starting and ending lines
+        For Each localvar As LocalVariableProperties In localVariables.Values
+            startingAndEndingLinesSet.Add(localvar.startingLineNum)
+            startingAndEndingLinesSet.Add(localvar.endingLineNum)
+        Next
+
+        'Convert starting and ending lines into sections
+        Dim startingAndEndingLines(startingAndEndingLinesSet.Count) As Integer
+        startingAndEndingLinesSet.CopyTo(startingAndEndingLines)
+        For i As Integer = 0 To startingAndEndingLines.Count - 2
+            scopeSections.Add(New SectionOfScope With {.startingLineNum = startingAndEndingLines(i), .endingLineNum = startingAndEndingLines(i + 1)})
+        Next
+
+        'Populate local variables for each section
+        For Each section In scopeSections
+            For Each localVar In localVariables.Values
+                If localVar.startingLineNum <= section.startingLineNum AndAlso localVar.endingLineNum >= section.endingLineNum Then
+                    section.variables.Add(localVar.name)
+
+                End If
+            Next
+        Next
+
+        Return scopeSections
+    End Function
+
+    Private Shared Sub PrintSections(ByVal sections As List(Of SectionOfScope), Optional ByVal suppressSubsetsOfSize As Integer = 0)
+        Console.WriteLine("Sections:")
+
+        Dim lastSectionSuppressed As Boolean = False
+
+        For Each section In sections
+            If section.variables.Any() Then
+                If section.endingLineNum - section.startingLineNum > suppressSubsetsOfSize Then
+                    PrintSectionExpanded(section, lastSectionSuppressed)
+
+                    lastSectionSuppressed = False
+                Else
+                    Console.Write(".")
+
+                    lastSectionSuppressed = True
+                End If
+            Else
+                Console.WriteLine("*********")
+            End If
+        Next
+    End Sub
+
+    Private Shared Sub PrintSectionExpanded(ByVal section As SectionOfScope, ByVal lastSuppressed As Boolean)
+        If lastSuppressed Then
+            Console.WriteLine()
+        End If
+
+        Console.Write("(" & section.startingLineNum & "-" & section.endingLineNum & ") ")
+        For Each variableName In section.variables
+            Console.Write(variableName & " ")
+        Next
+        Console.WriteLine()
+    End Sub
+End Class
+
+Friend Class LocalVariableProperties
+    Friend name As String
+    Friend startingLineNum As Integer
+    Friend startingLine As String
+    Friend endingLineNum As Integer
+End Class
+
+Friend Class SectionOfScope
+    Friend startingLineNum As Integer
+    Friend endingLineNum As Integer
+    Friend variables As New List(Of String)
+End Class
---
+++ Module1.vb
@@ -0,0 +1,96 @@
+﻿Imports System.IO
+
+Module Module1
+    Sub Main()
+        Dim regexTest = "Console.WriteLine(""Local Variables:"")"
+        Dim testText As String =
+            " Imports System.Text.RegularExpressions" & vbCrLf &
+" " & vbCrLf &
+" Public Class LocalVariableVisualizer" & vbCrLf &
+"     Public Shared Function Visualize(ByVal functionText As String) As String" & vbCrLf &
+"         Console.WriteLine(""Local Variables:"")" & vbCrLf &
+" " & vbCrLf &
+"         Dim lines As String() = functionText.Split(Environment.NewLine)" & vbCrLf &
+"         Dim foundLocals As New Dictionary(Of String, LocalVariableProperties)" & vbCrLf &
+"         Dim lineNumber As Integer = 0" & vbCrLf &
+"         Dim fullMatchRegex As String = String.Empty" & vbCrLf &
+" " & vbCrLf &
+"         For Each line As String In lines" & vbCrLf &
+"             line = line.Replace(Constants.vbLf, """")" & vbCrLf &
+"             If fullMatchRegex <> String.Empty Then" & vbCrLf &
+"                 For Each usage As Match In Regex.Matches(line, fullMatchRegex)" & vbCrLf &
+"                     foundLocals(usage.Groups(0).ToString()).endingLineNum = lineNumber" & vbCrLf &
+"                 Next" & vbCrLf &
+"             End If" & vbCrLf &
+" " & vbCrLf &
+"             Dim match = Regex.Match(line, ""Dim\s+(\w+)\s.*"")" & vbCrLf &
+"             If match.Length <> 0 Then" & vbCrLf &
+"                 Dim matchName = match.Groups(1).ToString()" & vbCrLf &
+"                 foundLocals.Add(matchName, New LocalVariableProperties() With" & vbCrLf &
+"                                        {.name = matchName, .startingLine = match.Groups(0).ToString(), .startingLineNum = lineNumber})" & vbCrLf &
+" this is a line of nothinnnnng" & vbCrLf &
+"                 fullMatchRegex = (fullMatchRegex & ""|("" & matchName & ""(?=[^\w]))|"" & matchName & ""$"").Trim(""|"")" & vbCrLf &
+"             End If" & vbCrLf &
+"             lineNumber += 1" & vbCrLf &
+"         Next" & vbCrLf &
+" " & vbCrLf &
+"         For Each localVariable In foundLocals" & vbCrLf &
+"             If localVariable.Value.endingLineNum = 0 Then" & vbCrLf &
+"                 localVariable.Value.endingLineNum = lineNumber" & vbCrLf &
+"             End If" & vbCrLf &
+" " & vbCrLf &
+"             Console.WriteLine(localVariable.Key & "" ("" & localVariable.Value.startingLineNum & ""-"" & localVariable.Value.endingLineNum & "")"")" & vbCrLf &
+"         Next" & vbCrLf &
+"         Console.WriteLine()" & vbCrLf &
+" " & vbCrLf &
+"         Console.WriteLine(""Sections:"")" & vbCrLf &
+"         Dim scopeSections As New List(Of SectionOfScope)" & vbCrLf &
+"         Dim startingAndEndingLines As New SortedSet(Of Integer)" & vbCrLf &
+"         For Each localvar As LocalVariableProperties In foundLocals.Values" & vbCrLf &
+"             startingAndEndingLines.Add(localvar.startingLineNum)" & vbCrLf &
+"             startingAndEndingLines.Add(localvar.endingLineNum)" & vbCrLf &
+"         Next" & vbCrLf &
+" " & vbCrLf &
+"         For i As Integer = 0 To startingAndEndingLines.Count - 2" & vbCrLf &
+"             scopeSections.Add(New SectionOfScope With {.startingLineNum = startingAndEndingLines(i), .endingLineNum = startingAndEndingLines(i + 1)})" & vbCrLf &
+"         Next" & vbCrLf &
+" " & vbCrLf &
+"         For Each section In scopeSections" & vbCrLf &
+"             For Each localVar In foundLocals.Values" & vbCrLf &
+"                 If localVar.startingLineNum <= section.startingLineNum AndAlso localVar.endingLineNum >= section.endingLineNum Then" & vbCrLf &
+"                     section.variables.Add(localVar.name)" & vbCrLf &
+"                 End If" & vbCrLf &
+"             Next" & vbCrLf &
+" " & vbCrLf &
+"             If section.variables.Any() Then" & vbCrLf &
+"                 Console.Write(""Section ("" & section.startingLineNum & ""-"" & section.endingLineNum & "") "")" & vbCrLf &
+"                 For Each variableName In section.variables" & vbCrLf &
+"                     Console.Write(variableName & "" "")" & vbCrLf &
+"                 Next" & vbCrLf &
+"                 Console.WriteLine()" & vbCrLf &
+"             Else" & vbCrLf &
+"                 Console.WriteLine(""*************"")" & vbCrLf &
+"             End If" & vbCrLf &
+"         Next" & vbCrLf &
+"         Return String.Empty" & vbCrLf &
+"     End Function" & vbCrLf &
+" End Class" & vbCrLf &
+" " & vbCrLf &
+" Friend Class LocalVariableProperties" & vbCrLf &
+"     Friend name As String" & vbCrLf &
+"     Friend startingLineNum As Integer" & vbCrLf &
+"     Friend startingLine As String" & vbCrLf &
+"     Friend endingLineNum As Integer" & vbCrLf &
+" End Class" & vbCrLf &
+" " & vbCrLf &
+" Friend Class SectionOfScope" & vbCrLf &
+"     Friend startingLineNum As Integer" & vbCrLf &
+"     Friend endingLineNum As Integer" & vbCrLf &
+"     Friend variables As New List(Of String)" & vbCrLf &
+" End Class"
+
+        LocalVariableVisualizer.Visualize(testText, 1, 7)
+        Console.ReadLine()
+    End Sub
+
+End Module
